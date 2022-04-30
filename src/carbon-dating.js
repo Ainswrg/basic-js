45083c2888888d015c2d68dc1f337cf6fc8cd714
Jaslan <Jaslan.dlnv@gmail.com>
2022-05-01 01:55:26 +0500

    implement tower of hanoi

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -28,8 +28,6 @@ function dateSample(sampleActivity) {
     return false;
   }
 
-  const MODERN_ACTIVITY = 15;
-  const HALF_LIFE_PERIOD = 5730;
   const k = 0.693 / HALF_LIFE_PERIOD;
   const t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
   return t;
